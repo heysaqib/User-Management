7dc81a3103d1b149dcdc8f13399fba339f9c5656
heysaqib <mailthatnerd@gmail.com>
2025-04-16 19:16:59 +0530

    Routes Handled

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express")
-const app = express()
+const app = express();
+const userRoutes = require("./routes/user")
+const adminRoutes = require("./routes/admin")
 
 
 
-app.get('/', (req,res)=>{
-    res.send("Hello World");
-})
+app.use("/user", userRoutes)
+app.use("/admin", adminRoutes);
+
+
+
+
+
+
+
 
 app.listen(5000,()=>{
     console.log("------------------------");
-    console.log("Server started");
+    console.log("Server started at : http://localhost:5000");
     console.log("------------------------");
 })
